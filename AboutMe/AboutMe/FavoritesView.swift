cd2905cac7eb55da584384884e8661a1def47eb8
DHLau <ldhtrythebest@163.com>
2025-06-18 16:41:18 +0900

    Update FavoritesView

--- AboutMe/AboutMe/FavoritesView.swift
+++ AboutMe/AboutMe/FavoritesView.swift
@@ -9,10 +9,55 @@ import SwiftUI
 
 struct FavoritesView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Favorites")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding(.bottom, 40)
+            
+            Text("Hobbies")
+                .font(.title2)
+            
+            HStack {
+                ForEach(information.hobbies, id: \.self) { hobby in
+                    Image(systemName: hobby)
+                        .resizable()
+                        .frame(maxWidth: 80, maxHeight: 60)
+                    
+                }
+                .padding()
+            }
+            .padding()
+
+            Text("Foods")
+                .font(.title2)
+            
+            HStack(spacing: 60) {
+                ForEach(information.foods, id: \.self) { food in
+                    Text(food)
+                        .font(.system(size: 48))
+                }
+            }
+            .padding()
+
+            Text("Favorite Colors")
+                .font(.title2)
+
+            HStack(spacing: 30) {
+                // 因为 ForEach 需要知道如何区分每一个项目，所以你需要传递 \.self 作为参数当做 id。
+                ForEach(information.colors, id: \.self) { color in
+                    color
+                        .frame(width: 70, height: 70)
+                        .cornerRadius(10)
+                }
+            }
+            .padding()
+        }
     }
 }
 
-#Preview {
-    FavoritesView()
+struct FavoritesView_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoritesView()
+    }
 }
