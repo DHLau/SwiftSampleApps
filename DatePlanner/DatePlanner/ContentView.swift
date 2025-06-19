d87b403d7926305cda867fc59d9314c96d548737
DHLau <ldhtrythebest@163.com>
2025-06-19 16:15:11 +0900

    DatePlannerApp init

---
+++ DatePlanner/DatePlanner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DatePlanner
+//
+//  Created by Eren on 2025/6/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DatePlanner/DatePlanner/DatePlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatePlannerApp.swift
+//  DatePlanner
+//
+//  Created by Eren on 2025/6/19.
+//
+
+import SwiftUI
+
+@main
+struct DatePlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
