b26efb5376f003c5eeffc0e1b8ab9fe00b9b451d
DHLau <ldhtrythebest@163.com>
2025-06-20 16:06:44 +0900

    grid init

---
+++ Grids/Grids/GridsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridsApp.swift
+//  Grids
+//
+//  Created by Eren on 2025/6/20.
+//
+
+import SwiftUI
+
+@main
+struct GridsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
