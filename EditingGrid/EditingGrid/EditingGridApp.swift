49b31a77060b45b4ad35bebdeb7b71ca2857b643
DHLau <ldhtrythebest@163.com>
2025-06-20 16:10:41 +0900

    editing grids commit

---
+++ EditingGrid/EditingGrid/EditingGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  EditingGridApp.swift
+//  EditingGrid
+//
+//  Created by Eren on 2025/6/20.
+//
+
+import SwiftUI
+
+@main
+struct EditingGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
